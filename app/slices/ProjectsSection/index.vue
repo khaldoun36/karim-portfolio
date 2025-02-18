7aa20312203a9fbb5f0b52bfe3464c260c93db5c
khaldoun36 <khaldoun36@gmail.com>
2025-02-18 08:36:20 +0400

    more bug fixes

--- app/slices/ProjectsSection/index.vue
+++ app/slices/ProjectsSection/index.vue
@@ -6,14 +6,14 @@
     <p class="mt-6 max-w-2xl text-lg text-pretty text-gray-400">
       {{ slice.primary.body }}
     </p>
-    <NuxtIsland name="Projects" />
+    <Projects />
   </Container>
 </template>
 
 <script setup lang="ts">
 import Container from "~/components/base/Container.vue";
+import Projects from "~/components/projects/Projects.vue";
 import type { Content } from "@prismicio/client";
-import { NuxtIsland } from "#components";
 
 // The array passed to `getSliceComponentProps` is purely optional.
 // Consider it as a visual hint for you when templating your slice.
